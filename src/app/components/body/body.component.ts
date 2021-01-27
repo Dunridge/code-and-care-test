dd11073b71643a9d6281645db248a8eeb00eec33
Dunridge <Polinka7max@gmail.com>
2021-01-27 14:01:19 +0200

    cleared up the comments

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -11,14 +11,10 @@ export class BodyComponent implements OnInit {
   inputLines: string[];
   result: string[];
 
-  // TODO: обратите внимание (многие ребята допускают ошибку на этом),  что нужно
-  //  вывести те строки, в которых хотя бы один из символов встречается ровно 2 раза,
-  //  использовать stdin и скинуть все на ideone.
 
   ngOnInit(): void {
     this.inputLines = ['asdf', 'fdas', 'asds', 'd fm', 'dfaa', 'aaaa', 'aabb', 'aaabb'];
     this.result = this.inputLines
-      // .filter(line => Object.keys(countBy(line)).forEach(elem => countBy(line)[elem] === 2));
       .filter(line => {
         for (const elem of Object.keys(countBy(line))) {
           if (countBy(line)[elem] === 2) {
